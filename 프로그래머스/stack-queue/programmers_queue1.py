980c2bbcbb4c36f7d23feff2f16badca81678ee1
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-09-06 22:55:31 +0900

    Modify: 프로그래머스 파일

--- 프로그래머스/stack-queue/programmers_queue1.py
+++ 프로그래머스/stack-queue/programmers_queue1.py
@@ -1,10 +1,12 @@
-from collections import deque 
-# 내 접근법
+
+# 내 접근법 
 # 1. progresses, speeds를 deque로 만든다.
 # 2. progresses를 한 번씩 돌면서 speeds의 해당 인덱스 값을 더한다. -> 값이 더해지는 한 턴에 모든 값들이 +를 해야한다고 생각함.
 # 3. 인덱스 0번 값이 100 이상인지 체크한다. 
 # 3. 100 이상인 경우 popleft 하고, count 한다. 
 # 4. 100 미만이면 다시 2번 과정으로 돌아가서 progresses의 length가 0이 될 때까지 반복한다.
+from collections import deque 
+
 def solution(progresses, speeds): 
     answer = [] 
     progresses = deque(progresses) 
@@ -43,6 +45,5 @@ print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
 print(solution1([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
 
 
-
-
-    
+# 문제 푸는데 걸린 시간 40분
+# => 문제를 풀때 하나하나의 조건을 다 표현하려 하기보단 역으로 생각하여 시간복잡도를 줄여나가야겠다. 
