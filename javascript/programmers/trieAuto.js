3350df20e1d3cb453315594000156fe07d2b013c
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-10-25 00:52:12 +0900

    programmers trie 자동완성 문제 완성

--- javascript/programmers/trieAuto.js
+++ javascript/programmers/trieAuto.js
@@ -1,7 +1,11 @@
+//1. trie구조를 사용하여 하나의 노드에 정점을 나타내는 value, 간선을 나타내는 children, 해당 정점을 지나가는 단어 개수 저장
+//2. 루트 노드부터 차례대로 자식노드로 내려오면서 해당 정점을 지나가는 단어의 개수가 1개 이하이면 count 값 반환 
+//3. 1 초과이면 자식 노드로 내려감 
 class Node {
     constructor(value = "") {
         this.value = value;
         this.children = new Map();
+        this.count = 0;
     }
 }
 
@@ -21,6 +25,8 @@ class Trie {
                 );
             }
             currentNode = currentNode.children.get(char);
+            currentNode.count += 1;
+
         }
     }
 
@@ -28,11 +34,12 @@ class Trie {
         let currentNode = this.root;
         let count = 0;
         for (const char of string) {
-            if (!(currentNode.children.size > 1)) {
+            count += 1;
+            if (!(currentNode.children.get(char).count > 1)) {
                 return count;
             }
             currentNode = currentNode.children.get(char);
-            count += 1;
+
         }
         return count;
 
@@ -51,5 +58,7 @@ function solution(words) {
     }
     return count
 }
-console.log(solution(["go", "gone", "guild"]))
-console.log(solution(["abc", "def", "ghi", "jklm"]))
+
+console.log("1번 답", solution(["go", "gone", "guild"]));
+console.log("2번 답", solution(["abc", "def", "ghi", "jklm"]));
+console.log("3번 답", solution(["word", "war", "warrior", "world"]));
