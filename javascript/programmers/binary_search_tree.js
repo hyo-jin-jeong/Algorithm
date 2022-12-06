c8e3e8f8064488eeeb7e03904fce6194682efa53
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-12-06 12:25:40 +0900

    programmers 이진탐색 array&tree 구현 방식

---
+++ javascript/programmers/binary_search_tree.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearch {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+    let currentNode = this.root;
+    while (currentNode !== null) {
+      if (currentNode.value < value) {
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          break;
+        }
+        currentNode = currentNode.right;
+      } else {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          break;
+        }
+        currentNode = currentNode.left;
+      }
+    }
+  }
+  has(value) {
+    let currentNode = this.root;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      if (currentNode.value < value) {
+        currentNode = currentNode.right;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+  }
+}
+const tree = new BinarySearch();
+tree.insert(5);
