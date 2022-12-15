dfc633040eab015fc049b79e1a1322010e245307
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-12-15 22:12:22 +0900

    programmers 조합 구현

---
+++ javascript/programmers/combination.js
@@ -0,0 +1,31 @@
+function combinations(arr, n) {
+  // 1개만 뽑는다면 그대로 조합을 반환한다. 탈출 조건으로도 사용된다.
+  console.log(arr, n);
+  if (n === 1) {
+    const re = arr.map((v) => [v]);
+    console.log(re);
+    return re;
+  }
+  const result = [];
+
+  // 요소를 순환한다
+  arr.forEach((fixed, idx, arr) => {
+    // 현재 index 이후 요소를 추출한다.
+    // index번째는 선택된 요소
+    const rest = arr.slice(idx + 1);
+    // 선택된 요소 이전 요소들을 제외하고 재귀 호출한다.
+    const combis = combinations(rest, n - 1);
+    // 선택된 요소와 재귀 호출을 통해 구한 조합을 합쳐준다.
+    console.log("combis", combis);
+    const combine = combis.map((v) => [fixed, ...v]);
+    console.log("combine", combine);
+    // 결과 값을 추가한다.
+    result.push(...combine);
+    console.log("result", result);
+  });
+
+  // 결과 반화
+  return result;
+}
+
+console.log(combinations([2, 3, 4, 1], 3));
